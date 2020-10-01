c9a11176cbd7380322d75497d77faaadd82a5b6f
MrFrozeN <bartekstafiej2@gmail.com>
2020-10-01 17:02:21 +0200

    generate user model with devise

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  acts_as_token_authenticatable
+
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def create_new_auhtentication_token
+    token = User.generate_unique_secure_token
+    update_atributes(authentication_token: token)
+  end
+end
