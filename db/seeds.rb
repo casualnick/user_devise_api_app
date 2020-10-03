e5c45dcb2d6ea8832a1147e8c1043f76c3013cba
MrFrozeN <bartekstafiej2@gmail.com>
2020-10-03 11:09:07 +0200

    migration of serializer book and review models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "Seeding 20 books"
+
+20.times do |n|
+  Book.create! title: Faker::Book.title,
+    author: Faker::Book.author,
+    image: Faker::Avatar.image
+end
+
+p "Seeding 10 users"
+10.times do |n|
+  User.create! email: Faker::Internet.email,
+    password: "test_user"
+end
+
+p "Seeding 100 reviews"
+100.times do |n|
+  Review.create!( 
+    user_id: Faker::Number.between(from: 1, to: 10),
+    book_id: Faker::Number.between(from: 1, to: 20),
+    title: "best book ever",
+    content_rating: Faker::Number.between(from: 1, to: 10),
+    recomment_rating: Faker::Number.between(from: 1, to: 10),
+    ) 
+end
