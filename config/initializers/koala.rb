ac7e9e4e80413bfba2f8410994e48409f2b82bf1
MrFrozeN <bartekstafiej2@gmail.com>
2020-10-03 11:01:29 +0200

    added api connection with facebook

---
+++ config/initializers/koala.rb
@@ -0,0 +1,8 @@
+Koala.configure do |config|
+#   config.access_token = MY_TOKEN
+#   config.app_access_token = MY_APP_ACCESS_TOKEN
+  config.app_id = "802603893827499"
+  config.app_secret = "c679ef23184e914c7042036a9d08ffe3"
+  # See Koala::Configuration for more options, including details on how to send requests through
+  # your own proxy servers.
+end
